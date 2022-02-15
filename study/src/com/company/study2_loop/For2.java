abdf9efa603e86ac68b4fdb7bc50618d5425f530
benggri <59782360+benggri@users.noreply.github.com>
2022-02-15 18:27:20 +0900

    Update For2.java

--- study/src/com/company/study2_loop/For2.java
+++ study/src/com/company/study2_loop/For2.java
@@ -6,10 +6,13 @@ public class For2 {
 
 //        중첩 for문
         for ( int i = 0 ; i < 5 ; i++ ) {
+            // 여기가 반복
             for ( int j = 0 ; j < 5 ; j++ ) {
+                // 여기가 반복
                 System.out.println( "i="+i+", j="+j );
             }
         }
+        // i가 1회 반복할 때 j는 5회 반복
 
     }
 }
