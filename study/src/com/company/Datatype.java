6244fdbc5191e99d332b9c73a4039153ce2da1d0
changheekim <benggri@gmail.com>
2022-02-15 14:08:02 +0900

    생성

---
+++ study/src/com/company/Datatype.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Datatype {
+
+    public static void main(String[] args) {
+// 숫자 타입
+// 정수형
+        int i = 0;
+        long l = 12345676987689876l;
+// 소수점
+        float f = 0.123f;
+        double d = 0.128379123d;
+
+// 문자형
+// 글자 단 하나!
+        char c = 'C'; // ''으로 써야함
+        String str = "글자가 여러개 wow"; // ""으로 써야함
+
+// 참/거짓 형
+        boolean bt = true;
+        boolean bf = false;
+
+    }
+}
---
+++ study/src/com/company/For.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class For {
+
+    public static void main(String[] args) {
+
+        for ( int i = 0 ; i < 5 ; i++ ) {
+            System.out.println("반복");
+        }
+
+//        for ( int i = 0 ; i < 5 ; i++ )
+//              i 는 0 으로 시작해서 ; 5보다 작을 때가지 반복 ; i는 1씩증가
+    }
+}
