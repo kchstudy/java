b37d07cdc37f5a14ea47464e521d5a82cf053449
benggri <59782360+benggri@users.noreply.github.com>
2022-02-16 06:33:08 +0900

    Update If.java

--- study/src/com/company/study3_if/If.java
+++ study/src/com/company/study3_if/If.java
@@ -1,1 +1,16 @@
 
+package com.company.study3_if;
+
+public class If {
+
+    public static void main(String[] args) {
+//        조건문
+        if ( 1 == 2 ) {
+            System.out.println( "if조건이 참이면" );
+        } else if ( 1 == 3 ) {
+            System.out.println( "if조건이 참이 아니고 else if조건이 참이면" );
+        } else {
+            System.out.println( "if조건이 참이 아니고 else if조건이 참이 아니면" );
+        }
+    }
+}
