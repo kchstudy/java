f9b4a7fa2c2d24753c01f34328ed437d23cf7870
changheekim <benggri@gmail.com>
2022-02-15 13:57:39 +0900

    자료형

---
+++ datatype/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+// 숫자 정수형
+        int i = 0;
+        long l = 123456;
+// 숫자 소수점
+        float f = 0.234f;
+        double d = 0.234544d;
+
+// 문자 한글자
+        char c = 'A'; // 글자 딱 하나만 됨
+        String str = "여러글자wow"; // 글자 여러개 쓸 수 있음
+
+// 참/거짓
+        boolean bt = true;
+        boolean bf = false;
+
+    }
+}
